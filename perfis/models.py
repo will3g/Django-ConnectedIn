d7ca56f778369ec5aa2d3a29eb158c44ad56634d
WillDafCom2 <willianpaulo_09@hotmail.com>
2019-09-29 16:20:31 -0300

    Variaveis no html e migrations para novos contatos

--- perfis/models.py
+++ perfis/models.py
@@ -5,6 +5,8 @@ class Perfil(models.Model):
     email = models.CharField(max_length=50, null=False)
     telefone = models.CharField(max_length=12, null=False)
     nome_empresa = models.CharField(max_length=30, null=False)
+    contatos = models.ManyToManyField('self')
+
     def convidar(self, perfil_convidado):
         Convite(solicitante=self, convidado=perfil_convidado).save()
 
@@ -12,6 +14,10 @@ class Convite(models.Model):
     solicitante = models.ForeignKey(Perfil, on_delete=models.PROTECT, related_name='convites_feitos')
     convidado = models.ForeignKey(Perfil, on_delete=models.PROTECT, related_name='convites_recebidos')
 
+    def aceitar(self):
+        self.convidado.contatos.add(self.solicitante)
+        self.solicitante.contatos.add(self.convidado)
+        self.delete()
 #####################################################
 # DE UMA MANEIRA SIMPLES E SEM SALVAR NO BD, FICARIA:
 # class Perfil(object):
--- perfis/views.py
+++ perfis/views.py
@@ -4,16 +4,27 @@ from perfis.models import Perfil
 from django.shortcuts import redirect
 
 def index(request):
-  return render(request,'index.html', {'perfis' : Perfil.objects.all()})
+  return render(
+    request,'index.html', 
+    {
+      'perfis' : Perfil.objects.all(),
+      'logado' : get_perfil_logado(request)
+    }
+  )
 
 def exibir(request, perfil_id):
   perfil = Perfil.objects.get(id=perfil_id)
   return render(request, 'perfil.html', {"perfil": perfil})
 
 def convidar(request, perfil_id):
-  perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+  convidar = Perfil.objects.get(id=perfil_id)
   logado = get_perfil_logado(request)
-  logado.convidar(perfil_a_convidar)
+  logado.convidar(convidar)
+  return redirect('index')
+
+def aceitar(request, convite_id):
+  convite = Convite.objects.get(id=convite_id)
+  convite.aceitar()
   return redirect('index')
 
 def get_perfil_logado(request):
